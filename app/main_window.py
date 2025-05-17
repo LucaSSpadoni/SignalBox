7aa1251eca43644aa0960ac9d78fc6da150b7717
Luca Spadoni <186880754+LucaSSpadoni@users.noreply.github.com>
2025-05-17 18:30:26 -0400

    Center widget in middle of screen

--- app/main_window.py
+++ app/main_window.py
@@ -1,8 +1,15 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # self.setWindowTitle("Main Window")
-        # self.setGeometry(100, 100, 800, 600)
-        # self.show()
+        self.setWindowTitle("Main Window")
+        self.setGeometry(200, 200, 800, 500)
+        self.center()
+        self.show()
+
+    def center(self):
+        frame = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        frame.moveCenter(centerPoint)
+        self.move(frame.topLeft())
