3c8f5bb639330756d5432c8816f1d56153aba8e6
Luca Spadoni <186880754+LucaSSpadoni@users.noreply.github.com>
2025-05-17 23:26:42 -0400

    Fixed button functionality

--- app/main_window.py
+++ app/main_window.py
@@ -12,7 +12,10 @@ class MainWindow(QMainWindow):
         self.setGeometry(200, 200, 800, 500)
         self.center()
         self.setStyleSheet("background-color: grey;")
+        self.stack = self.createStack()
         self.setCentralWidget(self.buildUI())
+
+        # show window
         self.show()
 
     def center(self):
@@ -28,18 +31,63 @@ class MainWindow(QMainWindow):
         titleLabel.setStyleSheet("color: purple; font-weight: bold;")
         return titleLabel
     
-    def buildPage(self, titleText):
+    def buildPitchShifterPage(self):
         page = QWidget()
         layout = QVBoxLayout()
+
+        # label logic
+        label = QLabel("Pitch Shifter", self)
+        label.setFont(QFont("Arial", 30))
+        label.setStyleSheet("color: purple; font-weight: bold;")
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        # Add pitch shifter controls here
         page.setLayout(layout)
         return page
     
+    def buildEqualizerPage(self):
+        page = QWidget()
+        layout = QVBoxLayout()
+
+        #label logic
+        label = QLabel("Equalizer", self)
+        label.setFont(QFont("Arial", 30))
+        label.setStyleSheet("color: purple; font-weight: bold;")
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        # Add equalizer controls here
+        page.setLayout(layout)
+        return page
+    
+    def buildSpectrogramPage(self):
+        page = QWidget()
+        layout = QVBoxLayout()
+
+        # label logic
+        label = QLabel("Spectrogram Visualizer", self)
+        label.setFont(QFont("Arial", 30))
+        label.setStyleSheet("color: purple; font-weight: bold;")
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        # Add spectrogram controls here
+        page.setLayout(layout)
+        return page
+    
+    def createStack(self):
+        stack = QStackedWidget()
+        stack.addWidget(self.buildSpectrogramPage())
+        stack.addWidget(self.buildEqualizerPage())
+        stack.addWidget(self.buildPitchShifterPage())
+        return stack
+
     def createNavBar(self):
         # create nav bar and add buttons
         navBar = QWidget()
         navLayout = QHBoxLayout()
         navLayout.setContentsMargins(10, 10, 10, 10)
-        #navBar.setSpacing(10)
         navBar.setStyleSheet("background-color: black;")
 
         navLayout.addWidget(self.titleLabel("SignalBox"), alignment=Qt.AlignLeft)
@@ -52,20 +100,13 @@ class MainWindow(QMainWindow):
 
         navBar.setLayout(navLayout)
         return navBar
-    
-    def createStack(self):
-        stack = QStackedWidget()
-        stack.addWidget(self.buildPage("Spectrogram Visualizer"))
-        stack.addWidget(self.buildPage("Equalizer"))
-        stack.addWidget(self.buildPage("Pitch Shifter"))
-        return stack
 
     def buildUI(self):
         # create container for nav bar and stack
         container = QWidget()
         layout = QVBoxLayout()
         layout.addWidget(self.createNavBar())
-        layout.addWidget(self.createStack())
+        layout.addWidget(self.stack)
         container.setLayout(layout)
         return container
         
